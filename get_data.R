b8ff1c4b5d1a6a8227598063903db0ac6c0f35e8
Daniel de Paula e Silva <danielpes91@gmail.com>
2015-04-26 13:19:30 -0300

    directory changes

--- get_data.R
+++ get_data.R
@@ -1,17 +1,13 @@
-if ( !file.exists("data") ) {
-    dir.create("data")
-}
-
-if ( !file.exists("data/dataset.zip") ) {
+if ( !file.exists("dataset.zip") ) {
     download.file(
         url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-        destfile = "data/dataset.zip"
+        destfile = "dataset.zip"
     )
 }
 
-if ( !file.exists("data/UCI HAR Dataset") ) {
+if ( !file.exists("UCI HAR Dataset") ) {
     unzip(
-        zipfile = "data/dataset.zip",
+        zipfile = "dataset.zip",
         exdir = "data"
     )
 }
